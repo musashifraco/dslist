362cac869eeb715255ca32a2c578fd47ab33abbd
musashifraco <ezequielestudos11@gmail.com>
2023-07-12 14:43:15 -0300

    Novo DTO para a entidade game criado. Criado novo end-point que faz busca na entidade Game pelo ID.

--- src/main/java/com/example/dslist/controllers/GameController.java
+++ src/main/java/com/example/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.example.dslist.controllers;
 
+import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.entities.Game;
 import com.example.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,17 @@ public class GameController {
     @Autowired
     public GameService gameService;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable(value = "id") Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
     }
+
+
 
 }
