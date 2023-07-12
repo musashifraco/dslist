f31f2ec0dd102a2ffff53f32caf51c2bb68ce7f0
musashifraco <ezequielestudos11@gmail.com>
2023-07-12 09:26:25 -0300

    Criação do controller, do repository, do service e do DTO.

---
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -0,0 +1,22 @@
+package com.example.dslist.services;
+
+import com.example.dslist.dto.GameMinDTO;
+import com.example.dslist.entities.Game;
+import com.example.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    public GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+}
